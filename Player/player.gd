80ccf5e3e061190f19e92281f26d42679fa7ef35
dzdn <jdustinwilliams@gmail.com>
2023-01-24 16:24:36 -0500

    Implement HitBox and HurtBox

--- Player/player.gd
+++ Player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 var movement_speed = 40.0
+var hp = 80
 @onready var sprite = $Sprite2D
 @onready var walkTimer = get_node("WalkTimer")
 
@@ -27,3 +28,8 @@ func movement():
 	velocity = mov.normalized() * movement_speed
 	move_and_slide()
 	
+
+
+func _on_hurt_box_hurt(damage):
+	hp -= damage
+	print(hp)
